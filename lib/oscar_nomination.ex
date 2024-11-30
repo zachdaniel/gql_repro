af843d3ab0e45426685eee31e261127cdac6857a
Oliver Severin Mulelid-Tynes <oliver@omt.tech>
2024-11-30 20:56:11 +0100

    excplicit int  mapping

--- lib/oscar_nomination.ex
+++ lib/oscar_nomination.ex
@@ -25,8 +25,19 @@ defmodule AshGraphql.Test.OscarNomination do
   end
 
   relationships do
-    belongs_to :movie, AshGraphql.Test.Movie, public?: true, writable?: true
-    belongs_to :nomination_location, AshGraphql.Test.Location, public?: true, writable?: true
-    belongs_to :celebration_location, AshGraphql.Test.Location, public?: true, writable?: true
+    belongs_to :movie, AshGraphql.Test.Movie,
+      public?: true,
+      writable?: true,
+      attribute_type: :integer
+
+    belongs_to :nomination_location, AshGraphql.Test.Location,
+      public?: true,
+      writable?: true,
+      attribute_type: :integer
+
+    belongs_to :celebration_location, AshGraphql.Test.Location,
+      public?: true,
+      writable?: true,
+      attribute_type: :integer
   end
 end
